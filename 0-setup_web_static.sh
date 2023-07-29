98295408445dea2b79b836e1d39f4f69925f2a81
Trisraki <trisraquel1@gmail.com>
2023-07-29 06:09:27 -0700

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,77 +1,18 @@
 #!/usr/bin/env bash
-# automated configuration of web server for web static
+# Script to set up web server for deployment
 
-# check if nginx is installed
-printf "checking if nginx is installed..."
-if ! command -v nginx &> /dev/null
-then
-	printf "nginx not installed\nTrying to install nginx...\n"
-	sudo apt-get update;
-	sudo apt-get install -y nginx;
-fi
-printf "nginx is installed\n"
-printf "starting nginx...\n"
-sudo service start nginx
-printf "nginx started!.\n"
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo service nginx start
 
-if [[ ! -d "/data" ]]
-then
-	printf "path /data does not exists, creating /data\n"
-	sudo mkdir /data/
-	printf "path /data created\n"
-else
-	printf "path /data already exists\n"
-fi
+default_sites="/etc/nginx/sites-available/default"
+location=$(grep -Fn location $default_sites | head -1 | cut -d":" -f1)
+hbnb_static="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 
-if [[ ! -d "/data/web_static/" ]]
-then
-        printf "path /data/web_static/ does not exists, creating /data/web_static\n"
-        sudo mkdir -p /data/web_static/
-        printf "path /data/web_static/ created\n"
-else
-        printf "path /data/web_static/ already exists\n"
-fi
+sudo mkdir -p "/data/web_static/releases/test/" "/data/web_static/shared/"
+echo "<h1>Hello World!</h1>" | sudo tee "/data/web_static/releases/test/index.html"
+sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
+sudo chown -hR ubuntu:ubuntu "/data/"
 
-if [[ ! -d "/data/web_static/releases/" ]]
-then
-        printf "path /data/web_static/releases/ does not exists, creating /data/web_static/releases\n"
-        sudo mkdir -p /data/web_static/releases/
-        printf "path /data/web_static/releases/ created\n"
-else
-        printf "path /data/web_static/releases/ already exists\n"
-fi
-
-if [[ ! -d "/data/web_static/shared" ]]
-then
-        printf "path /data/web_static/shared does not exists, creating /data/web_static/shared\n"
-        sudo mkdir -p /data/web_static/shared
-        printf "path /data/web_static/shared created\n"
-else
-        printf "path /data/web_static/shared already exists\n"
-fi
-
-if [[ ! -d "/data/web_static/releases/test" ]]
-then
-        printf "path /data/web_static/releases/ does not exists, creating /data/web_static/releases/test\n"
-        sudo mkdir -p /data/web_static/releases/test
-        printf "path /data/web_static/releases/test created\n"
-else
-        printf "path /data/web_static/releases/test already exists\n"
-fi
-
-printf "creating test file, index.html\n"
-printf "Hello world" | sudo tee /data/web_static/releases/test/index.html
-printf "created /data/web_static/releases/test/index.html\n"
-
-printf "creating symlink /data/web_static/current\n"
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-printf "created symlink /data/web_static/current\n"
-
-printf "changing ownership of /data to ubuntu:ubuntu\n"
-sudo chown -R ubuntu:ubuntu /data
-sudo chown -R ubuntu:ubuntu /data/*
-printf "user and group of /data changed to ubuntu:ubuntu\n"
-
-sudo sed -i '53i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
-
+sudo sed -i "${location}i ${hbnb_static}" "${default_sites}"
 sudo service nginx restart
